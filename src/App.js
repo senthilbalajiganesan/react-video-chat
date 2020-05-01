9053b2c03ce0e846f25f4e44a5ddd39a2b4d81f9
SenthilBalaji <senthil.g.balaji@gmail.com>
2020-05-01 19:10:57 +0530

    testin CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Creating a Video Chat Application
         </a>
       </header>
     </div>
